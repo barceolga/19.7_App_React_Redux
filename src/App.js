ff0faa837b8d78a68f9fbe37a41c563a19b0cd3c
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-25 18:50:03 +0200

    Update content for App.js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 const App = () => {
     return (
       <div className="App">
+          <h1> The comments app </h1>
+          <h2> The comments list</h2>
           <CommentsListContainer/>
       </div>
     );

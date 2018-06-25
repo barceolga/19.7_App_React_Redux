d2e4d21f80f6183ff94bc7f8b4dc65884897f0fc
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-25 10:13:59 +0200

    Update content and fix bugs for Comment.js file

--- src/Comment.js
+++ src/Comment.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {thumbUpComment} from './index.js';
+import {thumbDownComment} from './index.js';
 
-const Comment = ({text, votes}) => <li>{text} <span>votes: {votes}</span></li>;
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) =>
+<li>
+    {text} <span>votes: {votes}</span>
+    <button onClick={() => thumbUpComment(id)}>Thumb up</button>
+    <button onClick={() => thumbDownComment(id)}>Thumb down</button>
+</li>;
 
 export default Comment;

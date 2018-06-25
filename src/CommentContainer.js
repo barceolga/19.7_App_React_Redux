b764735e2f4933c462123410097b23129245a1d6
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-25 18:51:25 +0200

    Update content for CommentContainer.js

--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -2,10 +2,12 @@ import {connect} from 'react-redux';
 import Comment from './Comment.js';
 import {thumbUpComment} from './index.js';
 import {thumbDownComment} from './index.js';
+import {removeComment} from './index.js';
 
 const mapDispatchToProps = dispatch => ({
   thumbUpComment: (id) => dispatch(thumbUpComment(id)),
-  thumbDownComment: (id) => dispatch(thumbDownComment(id))
+  thumbDownComment: (id) => dispatch(thumbDownComment(id)),
+  removeComment: (id) => dispatch(removeComment(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);

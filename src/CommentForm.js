6a05b2a1de7fdee0baf34d8ff0deac41cd2fd8c6
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-26 15:51:08 +0200

    Add Local Form component to CommentForm file

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,40 +1,34 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { LocalForm, Control, actions } from 'react-redux-form';
 import {addComment} from './index.js';
 
-const HandleSubmit = addComment;
-const validate = values => {
-  const errors= {}
-  if (!values.commentField) {
-    errors.commentField ='Required'
-  } else if (values.commentField.lenght > 350) {
-    errors.commentField = 'Must be 350 characters or less'
+ export default class CommentForm extends React.Component {
+  handleChange(values) {
+
+  }
+
+  handleSubmit(values) {
+    this.props.addComment({
+      values: this.props.values
+    })
+  }
+  /*attachDispatch(dispatch) {
+    this.formDispatch = dispatch;
+  }
+  changeTextToFormText() {
+    this.formDispatch(actions.change('comment.text', model.value));
+  }*/
+  render() {
+    return (
+      <LocalForm
+       model="comment"
+      // getDispatch{(dispatch) => this.attach(dispatch)}
+      // initialState={{text: ''}}
+       onChange={(values) => this.handleChange(values)}
+       onSubmit={(values) => this.handleSubmit(values)}
+       >
+         <Control.text model=".comment" />
+     </LocalForm>
+   );
   }
 }
-
-const renderField = ({input, label, type, meta: {touched, error}}) => (
-  <div>
-      <label>{label}</label>
-      <div>
-          <input {...input} placeholder={label} type={type}/>
-          {touched &&(error&& <span>{error}</span>)}
-      </div>
-  </div>
-)
-const CommentForm = (props) => {
-  const {handleSubmit, reset} = props
-  return (
-    <form onSubmit={handleSubmit}>
-      <Field name="commentField" type="text" component={renderField} label="Comment field"/>
-      <div>
-        <button type="submit">Submit</button>
-        <button type="button" onclick={reset}>Clear Values</button>
-      </div>
-    </form>
-  )
-}
-
-export default CommentForm({
-  form: 'commentForm',
-  validate
-})(CommentForm)

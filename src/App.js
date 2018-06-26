448694edb3a80beae93668f9dc19daead9c1c7eb
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-26 09:14:54 +0200

    Add new componentes to App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CommentsListContainer from './CommentsListContainer';
+import CommentFormContainer from './CommentFormContainer';
 import './App.css';
 
 const App = () => {
     return (
       <div className="App">
           <h1> The comments app </h1>
+          <h2>Write your comment here</h2>
+          <CommentFormContainer />
           <h2> The comments list</h2>
           <CommentsListContainer/>
       </div>

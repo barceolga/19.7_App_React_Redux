6694729e92b0e085beb2e7bd068b6e04c9083bd4
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-26 15:59:56 +0200

    Update content for CommentFormContainer.js file

--- src/CommentFormContainer.js
+++ src/CommentFormContainer.js
@@ -1,9 +1,15 @@
 import {connect} from 'react-redux';
 import CommentForm from './CommentForm';
 import CommentsList from './CommentsList';
+import {addComment} from './index.js';
+import Comment from './Comment.js';
 
 const mapStateToProps = state => ({
-  comments: [state.comments, ...comment]
+  comments: [...state.comments, Comment]
 })
 
-export default connect(mapStateToProps)(CommentsList);
+const mapDispatchToProps = dispatch => ({
+  addComment: (text) => dispatch(addComment(text))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentsList);

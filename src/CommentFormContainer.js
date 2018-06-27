d16d2a15ec6b6640360e45eeafc6be2b23bb2d20
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-27 15:58:56 +0200

    Update content and fix bugs for CommentFormContainer.js file

--- src/CommentFormContainer.js
+++ src/CommentFormContainer.js
@@ -1,34 +1,22 @@
 import {connect} from 'react-redux';
-import React from 'react';
 import { reduxForm} from 'redux-form';
+import React from 'react';
 import CommentForm from './CommentForm';
 import CommentsList from './CommentsList';
-import {addComment} from './actions.js';
 import Comment from './Comment.js';
+import {addComment} from './actions.js';
 
-class CommentFormContainer extends React.Component {
-  submit = values => {
-    const text = JSON.stringify(values);
-    console.log(text);
-    return text;
-  }
-  render() {
-    return <CommentForm onSubmit={this.submit} />
-  }
-};
+let CommentFormContainer = ({addComment, values}) =>
+  <CommentForm
+    onSubmit={values => addComment(values.CommentFormContainer)}
+  />
 
-const newComment = dispatch => ({
-    addComment: (submit) => dispatch(addComment(submit))
+  const mapDispatchToProps = (dispatch) => ({
+    addComment: value =>dispatch(addComment(value))
   });
-const mapStateToProps = state => ({
-    comments: [...state.comments, newComment]
+  console.log(mapDispatchToProps);
+  const mapStateToProps = state => ({
+    comments: [...state.comments, mapDispatchToProps]
   });
 
-CommentFormContainer = connect(
-  mapStateToProps,
-  newComment,
-)(CommentFormContainer);
-
-export default reduxForm({
-  form: 'commentFormContainer'
-})(CommentFormContainer);
+  export default reduxForm({ form: 'CommentFormContainer'})(connect(mapStateToProps, mapDispatchToProps)(CommentFormContainer));

e71ba8b29300b13ff9446351a155077dd82ed580
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-27 15:58:20 +0200

    Fix bugs and update content for CommentForm.js file

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { reduxForm, Field} from 'redux-form';
-import {addComment} from './index.js';
 
-let CommentForm = props => {
-  const { handleSubmit } = props;
+
+const CommentForm = ({ handleChange, handleSubmit, value}) => {
   return <form onSubmit={handleSubmit} className="form">
       <div className="field">
-        <label className="label">Comment field</label>
-        <Field className="textearea" name="commentField" component="textarea" rows="7" cols="70" />
+        <Field className="textearea" name="commentField" component="textarea" rows="7" cols="70" onChange={handleChange} value={value} />
       </div>
       <div className="field">
-        <div className="control">
           <button className="commentButton" type="submit">Add comment</button>
-        </div>
       </div>
     </form>
 };
-CommentForm = reduxForm({
+
+export default reduxForm({
   form: 'commentForm'
 })(CommentForm);
-
-export default CommentForm;

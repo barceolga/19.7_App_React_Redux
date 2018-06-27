773d19a4c58060b317737316a518421f8c07e110
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-27 10:44:38 +0200

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CommentsListContainer from './CommentsListContainer';
 import CommentFormContainer from './CommentFormContainer';
+import {addComment} from './actions.js';
 import './App.css';
 
-const App = () => {
+  const App= () => {
     return (
       <div className="App">
           <h1> The comments app </h1>
           <h2>Write your comment here</h2>
-          <CommentFormContainer />
+          //<CommentFormContainer />
           <h2> The comments list</h2>
           <CommentsListContainer/>
       </div>
     );
-};
+  };
+
 
 export default App;

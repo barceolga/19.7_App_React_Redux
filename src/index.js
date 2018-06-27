5827f5e916c033df9fa5035adb055b7d80db7774
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-27 10:52:26 +0200

    Delete rootReducer form index.js file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { reducer as formReducer} from 'redux-form';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-//import basicReducer from './reducers.js';
+import { createStore} from 'redux';
+import rootReducer from './reducers.js';
 import DevTools from './DevTools';
 import {addComment} from './actions.js'
-import comments from './comments.js';
-import users from './users.js';
-
-const rootReducer = combineReducers({
-  form: formReducer,
-  users,
-  comments
-})
 
 const store = createStore(rootReducer);
 

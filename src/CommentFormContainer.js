8709dbe1359e8bf494f6a9c3d2bb2b75af618d62
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-27 10:46:43 +0200

    Update content for CommentFormContainer.js file

--- src/CommentFormContainer.js
+++ src/CommentFormContainer.js
@@ -1,15 +1,34 @@
 import {connect} from 'react-redux';
+import React from 'react';
+import { reduxForm} from 'redux-form';
 import CommentForm from './CommentForm';
 import CommentsList from './CommentsList';
-import {addComment} from './index.js';
+import {addComment} from './actions.js';
 import Comment from './Comment.js';
 
+class CommentFormContainer extends React.Component {
+  submit = values => {
+    const text = JSON.stringify(values);
+    console.log(text);
+    return text;
+  }
+  render() {
+    return <CommentForm onSubmit={this.submit} />
+  }
+};
+
+const newComment = dispatch => ({
+    addComment: (submit) => dispatch(addComment(submit))
+  });
 const mapStateToProps = state => ({
-  comments: [...state.comments, Comment]
-})
+    comments: [...state.comments, newComment]
+  });
 
-const mapDispatchToProps = dispatch => ({
-  addComment: (text) => dispatch(addComment(text))
-})
+CommentFormContainer = connect(
+  mapStateToProps,
+  newComment,
+)(CommentFormContainer);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CommentsList);
+export default reduxForm({
+  form: 'commentFormContainer'
+})(CommentFormContainer);

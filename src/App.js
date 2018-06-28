9eefeaa1bbee3e32f20b347127b630a48eac57b9
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-28 16:06:00 +0200

    Update content for App.js according to App.css style guide

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import './App.css';
   const App= () => {
     return (
       <div className="App">
-          <h1> The comments app </h1>
-          <h2>Write your comment here</h2>
+          <h1 className="App-title"> The comments app </h1>
+          <h2 className="App-intro">Write your comment here</h2>
           <ContainerForm/>
-          <h2> The comments list</h2>
+          <h2 className="App-intro"> The comments list</h2>
           <CommentsListContainer/>
       </div>
     );

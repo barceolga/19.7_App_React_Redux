5f35ec9d1d4f4dc316a5c3180da4b35ffeec83a5
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-28 15:24:51 +0200

    Update content for Comment.Form.js file

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import { reduxForm, Field} from 'redux-form';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import CommentsList from './CommentsList';
+import Comment from './Comment.js';
+import {addComment} from './actions.js';
 
-
-const CommentForm = ({ handleChange, handleSubmit, value}) => {
-  return <form onSubmit={handleSubmit} className="form">
-      <div className="field">
-        <Field className="textearea" name="commentField" component="textarea" rows="7" cols="70" onChange={handleChange} value={value} />
-      </div>
-      <div className="field">
-          <button className="commentButton" type="submit">Add comment</button>
-      </div>
-    </form>
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addComment: text =>dispatch(addComment(text)),
+  }
 };
 
-export default reduxForm({
-  form: 'commentForm'
-})(CommentForm);
+const mapStateToProps = state => ({
+  comments: [...state.comments, mapDispatchToProps]
+});
+
+class CommentForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event){
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = this.state.name;
+    this.props.addComment( text );
+    this.setState({ text: ""});
+  }
+
+  render() {
+    const text = this.state.name;
+    return (
+      <form onSubmit ={this.handleSubmit}>
+          <div className="form-grooup">
+              <textarea
+                rows="7"
+                cols="70"
+                value = {this.state.name}
+                onChange={this.handleChange}
+              />
+        </div>
+            <button type="submit">Add comment</button>
+        </form>
+    )
+  }
+}
+
+const ContainerForm = connect(mapStateToProps, mapDispatchToProps)(CommentForm);
+
+export default ContainerForm

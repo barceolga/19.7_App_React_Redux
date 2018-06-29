c91741fc18c867a85da6aa7c67bd35e74b084498
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-29 09:11:36 +0200

    Update comments for code in formReducer.js file

--- src/formReducer.js
+++ src/formReducer.js
@@ -2,10 +2,11 @@
 /*jshint esversion: 6*/
 // Importing the necessary action's types:
 import {
-  UPDATE_COMMENT
+  UPDATE_COMMENT,
+  EDIT_COMMENT
 } from './actions';
-
-// Creating a reducer for administrating comments' state
+import comments from './comments.js'
+// Creating a reducer for administrating form's state
 
 export default function formReducer (state = {id: "", text: ""}, action) {
     switch(action.type) {

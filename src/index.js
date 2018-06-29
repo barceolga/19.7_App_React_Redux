d01529baa37e5cfaeef33e12b3ebcc0f2552a7fa
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-29 12:46:39 +0200

    Update index.js file by deleting unnecesary code

--- src/index.js
+++ src/index.js
@@ -6,13 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers.js';
-import {
-  ADD_COMMENT,
-  REMOVE_COMMENT,
-  EDIT_COMMENT,
-  THUMB_UP_COMMENT,
-  THUMB_DOWN_COMMENT
-} from './actions';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';

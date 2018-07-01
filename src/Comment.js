6eb0f4403d7078062d53c108c7456dd19f037378
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-01 14:22:05 +0200

    Update content for Comment.js file

--- src/Comment.js
+++ src/Comment.js
@@ -15,19 +15,19 @@ const Comment = ({
 }) => {
  return (
       <li className="Comment-item">
-          <div>
+          <div className="Comment-item_common">
               <p className="Comment-item_paragraph">{text}</p>
-          </div>
-          <div className="evaluation">
-              <div className="evaluation-positive"> <span className="positive">{positive}</span></div>
-              <div className="evaluation-total"><span className="total">Total:{votes}</span></div>
-              <div className="evaluation-negative"><span className="negative">{negative}</span></div>
+              <div className="evaluation">
+                  <div className="evaluation-positive"> <span className="positive">{positive}</span></div>
+                  <div className="evaluation-total"><span className="total">Sum: {votes}</span></div>
+                  <div className="evaluation-negative"><span className="negative">{negative}</span></div>
+              </div>
           </div>
           <div className="Comment-group">
                   <button className="Comment-group_button" onClick={() => thumbUpComment(id)}><FontAwesomeIcon icon="thumbs-up" /></button>
                   <button className="Comment-group_button_negative" onClick={() => thumbDownComment(id)}><FontAwesomeIcon icon="thumbs-down" /></button>
-                  <button className="Comment-group_button" onClick={() => updateComment(id, text)}>Update</button>
-                  <button className="Comment-group_button_negative" onClick={() => removeComment(id)}>Delete</button>
+                  <button className="Comment-group_button" onClick={() => updateComment(id, text)}><FontAwesomeIcon icon="edit" /></button>
+                  <button className="Comment-group_button_negative" onClick={() => removeComment(id)}><FontAwesomeIcon icon="trash" /></button>
           </div>
       </li>
   );

ee3ec94244e5224fb973c85497b1297da7c6ef4d
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-01 11:31:04 +0200

    Add new functionalities in Comment.js file

--- src/Comment.js
+++ src/Comment.js
@@ -4,21 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Comment = ({
   text,
+  positive,
+  negative,
   votes,
   id,
   thumbUpComment,
   thumbDownComment,
   removeComment,
   updateComment
-}) =>
-<li className="Comment-item">
-    {text} <p>votes: <span>{votes}</span></p>
-    <div className="Comment-group">
-        <button className="Comment-group_button" onClick={() => thumbUpComment(id)}><FontAwesomeIcon icon="thumbs-up" /></button>
-        <button className="Comment-group_button" onClick={() => thumbDownComment(id)}><FontAwesomeIcon icon="thumbs-down"  /></button>
-        <button className="Comment-group_button" onClick={() => removeComment(id)}>Delete</button>
-        <button className="Comment-group_button" onClick={() => updateComment(id, text)}>Update</button>
-    </div>
-</li>;
+}) => {
+ return (
+      <li className="Comment-item">
+          <div>
+              <p className="Comment-item_paragraph">{text}</p>
+          </div>
+          <div className="evaluation">
+              <div className="evaluation-positive"> <span className="positive">{positive}</span></div>
+              <div className="evaluation-total"><span className="total">Total:{votes}</span></div>
+              <div className="evaluation-negative"><span className="negative">{negative}</span></div>
+          </div>
+          <div className="Comment-group">
+                  <button className="Comment-group_button" onClick={() => thumbUpComment(id)}><FontAwesomeIcon icon="thumbs-up" /></button>
+                  <button className="Comment-group_button_negative" onClick={() => thumbDownComment(id)}><FontAwesomeIcon icon="thumbs-down" /></button>
+                  <button className="Comment-group_button" onClick={() => updateComment(id, text)}>Update</button>
+                  <button className="Comment-group_button_negative" onClick={() => removeComment(id)}>Delete</button>
+          </div>
+      </li>
+  );
+}
 
 export default Comment;

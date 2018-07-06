ac2f14bbcd9f4fb1b0340844e928767c859385c9
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-06 08:20:48 +0200

    Update content for Comment.js file

--- src/Comment.js
+++ src/Comment.js
@@ -21,13 +21,12 @@ const Comment = ({
                   <div className="evaluation-total">Votes:<span style={style} className={`total ${votes>=0?'positive':'negative'}`}> {votes}</span></div>
               </div>
               <p className="Comment-item_paragraph">{text}</p>
-
-          </div>
-          <div className="Comment-group">
-                  <button className="Comment-group_button" onClick={() => thumbUpComment(id)}><FontAwesomeIcon icon="thumbs-up" /></button>
-                  <button className="Comment-group_button_negative" onClick={() => thumbDownComment(id)}><FontAwesomeIcon icon="thumbs-down" /></button>
-                  <button className="Comment-group_button" onClick={() => updateComment(id, text)}><FontAwesomeIcon icon="edit" /></button>
-                  <button className="Comment-group_button_negative" onClick={() => removeComment(id)}><FontAwesomeIcon icon="trash" /></button>
+              <div className="Comment-group">
+                      <button className="Comment-group_button" onClick={() => thumbUpComment(id)}><FontAwesomeIcon icon="thumbs-up" /></button>
+                      <button className="Comment-group_button_negative" onClick={() => thumbDownComment(id)}><FontAwesomeIcon icon="thumbs-down" /></button>
+                      <button className="Comment-group_button" onClick={() => updateComment(id, text)}><FontAwesomeIcon icon="edit" /></button>
+                      <button className="Comment-group_button_negative" onClick={() => removeComment(id)}><FontAwesomeIcon icon="trash" /></button>
+              </div>
           </div>
       </li>
   );
